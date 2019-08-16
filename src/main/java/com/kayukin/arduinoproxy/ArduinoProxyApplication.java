383a95c018e92a1811367b32e81d0d0bee0f750f
Константин <kayukin@gmail.com>
2019-08-16 15:03:13 +0600

    Change http library

--- src/main/java/com/kayukin/arduinoproxy/ArduinoProxyApplication.java
+++ src/main/java/com/kayukin/arduinoproxy/ArduinoProxyApplication.java
@@ -1,6 +1,5 @@
 package com.kayukin.arduinoproxy;
 
-import kong.unirest.Unirest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -11,7 +10,6 @@ public class ArduinoProxyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ArduinoProxyApplication.class, args);
-        Unirest.shutDown();
     }
 
 }
--- src/main/java/com/kayukin/arduinoproxy/connection/ThingSpeakConnection.java
+++ src/main/java/com/kayukin/arduinoproxy/connection/ThingSpeakConnection.java
@@ -1,25 +1,29 @@
 package com.kayukin.arduinoproxy.connection;
 
 import com.kayukin.arduinoproxy.model.SensorsData;
-import kong.unirest.HttpResponse;
-import kong.unirest.Unirest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
-@Service
 public class ThingSpeakConnection {
     private static final Logger log = LoggerFactory.getLogger(ThingSpeakConnection.class);
     private static final String API_URL = "https://api.thingspeak.com/update";
+    private final RestTemplate restTemplate;
+
+    public ThingSpeakConnection(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
     public void sendSensorsData(SensorsData sensorsData) {
-        HttpResponse<String> response = Unirest.get(API_URL)
-                .queryString("api_key", "EQEGDGU0V5OB5IAY")
-                .queryString("field1", sensorsData.getTemperature())
-                .queryString("field2", sensorsData.getHumidity())
-                .asString();
-        if (!response.isSuccess()) {
-            log.error("Failed to send sensors data: {}", response.getStatusText());
+        ResponseEntity<Object> response = restTemplate.getForEntity(UriComponentsBuilder.fromHttpUrl(API_URL)
+                .queryParam("api_key", "EQEGDGU0V5OB5IAY")
+                .queryParam("field1", sensorsData.getTemperature())
+                .queryParam("field2", sensorsData.getHumidity())
+                .toUriString(), Object.class);
+        if (!response.getStatusCode().is2xxSuccessful()) {
+            log.error("Failed to send sensors data: {}", response.getStatusCode());
         }
     }
 }
